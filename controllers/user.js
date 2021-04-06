e8a4eb95deaf5499b3b4851e26f3d1133b0bf830
gmlwhd6159 <gmlwhd6159@gmail.com>
2021-04-06 11:34:05 +0900

    Add #6 -edit: oauth

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,6 @@
 const models = require("../models");
 const { user, logo, preset } = models;
 const jwt = require("jsonwebtoken");
-
 require("dotenv").config();
 
 module.exports = {
@@ -24,5 +23,77 @@ module.exports = {
           res.status(200).json({ message: "ok" });
         });
     }
+  },
+
+  signin: async (req, res) => {
+    const userInfo = await user.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (!userInfo) {
+      res.status(404).json({ message: "undefined" });
+    } else {
+      if (userInfo.dataValues.password !== req.body.password) {
+        res.status(404).json({ message: "fail" });
+      } else {
+        delete userInfo.dataValues.password;
+        const accessToken = jwt.sign(userInfo.dataValues, process.env.ACCESS_SECRET, {
+          expiresIn: "1h",
+        });
+        res.status(200).json({ accessToken: accessToken, message: "ok" });
+      }
+    }
+  },
+
+  signout: async (req, res) => {
+    res.status(200).json({ message: "ok" });
+  },
+
+  editpw: async (req, res) => {
+    const accessToken = req.body.accessToken;
+    const userInfo = jwt.verify(accessToken, process.env.ACCESS_SECRET);
+    const users = await user.findOne({
+      where: {
+        email: userInfo.email,
+      },
+    });
+    if (req.body.password !== users.dataValues.password) {
+      res.status(404).json({ message: "check agin" });
+    } else {
+      user
+        .update(
+          {
+            password: req.body.editpw,
+          },
+          { where: { email: userInfo.email } },
+        )
+        .then(() => {
+          res.status(200).json({ message: "ok" });
+        });
+    }
+  },
+
+  userinfo: async (req, res) => {
+    const accessToken = req.body.accessToken;
+    const userinfo = jwt.verify(accessToken, process.env.ACCESS_SECRET);
+
+    res.status(200).json({ message: "ok", data: userinfo });
+  },
+
+  deleteid: async (req, res) => {
+    const accessToken = req.body.accessToken;
+    const userInfo = jwt.verify(accessToken, process.env.ACCESS_SECRET);
+
+    user
+      .destroy({
+        where: {
+          email: userInfo.email,
+        },
+      })
+      .then(() => {
+        res.status(200).json({ message: "ok" });
+      });
   },
 };
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const http = require("http");
 const https = require("https");
 const cookieParser = require("cookie-parser");
 const user = require("./controllers/user");
-
+const callback = require("./controllers/callback");
 const app = express();
 
 app.use(cors({}));
@@ -15,6 +15,12 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.post("/user/signup", user.signup);
+app.post("/user/signin", user.signin);
+app.post("/user/signout", user.signout);
+app.post("/user/editpw", user.editpw);
+app.post("/user/userinfo", user.userinfo);
+app.post("/user/deleteid", user.deleteid);
+app.post("/callback");
 
 http.createServer(app).listen(5000, () => {
   console.log("server on 5000");
